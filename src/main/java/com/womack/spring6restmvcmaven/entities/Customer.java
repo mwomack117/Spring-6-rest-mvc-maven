30a4659ac3992264c2a3d7579aac62b173a673ee
Michael Womack <michael.womack84@gmail.com>
2024-09-18 23:24:07 -0400

    add flyway with migration scripts and update application properties to use h2 when running integration tests

--- src/main/java/com/womack/spring6restmvcmaven/entities/Customer.java
+++ src/main/java/com/womack/spring6restmvcmaven/entities/Customer.java
@@ -2,8 +2,8 @@ package com.womack.spring6restmvcmaven.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
@@ -18,13 +18,15 @@ import java.util.UUID;
 public class Customer {
 
     @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @UuidGenerator
     @JdbcTypeCode(SqlTypes.CHAR)
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
     private String firstName;
     private String lastName;
+
+    @Column
+    private String email;
 
     @Version
     private Integer version;
